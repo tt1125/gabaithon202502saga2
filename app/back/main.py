cf3f38d57947978efca4ee0021c4e59a23d06246
tt1125 <tsurutsuru275@gmail.com>
2025-02-27 21:23:47 +0900

    Flaskインポートを整理し、jsonifyおよびrequestを追加しました

--- app/back/main.py
+++ app/back/main.py
@@ -1,4 +1,9 @@
-from flask import Flask, send_from_directory
+from flask import (
+    Flask,
+    jsonify,
+    request,
+    send_from_directory,
+)
 from flask_cors import CORS
 import os
 from flask_sqlalchemy import SQLAlchemy
