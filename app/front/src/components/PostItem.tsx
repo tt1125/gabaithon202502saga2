b920b1ad85680f6d1363bc6ebb42b9ed597fdf46
tt1125 <tsurutsuru275@gmail.com>
2025-02-28 11:37:24 +0900

    SSGおよびビルドマニフェストの生成ファイルを追加し、PostItemコンポーネントにルートポイントの表示を追加

--- app/front/src/components/PostItem.tsx
+++ app/front/src/components/PostItem.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { PostData } from "@/app/social/page";
 import { Avatar, Box, Typography, Paper } from "@mui/material";
+import { Flag, Home, MapPin, Navigation } from "lucide-react";
 
 type PostItemProps = {
   postData: PostData;
@@ -74,9 +75,163 @@ export default function PostItem({ postData }: PostItemProps) {
           >
             {postData.title}
           </Typography>
-          <Typography variant="body2" sx={{ color: "text.secondary" }}>
+          <Typography variant="body2" sx={{ color: "text.secondary", mb: 2 }}>
             {postData.comment}
           </Typography>
+
+          {/* ルートポイントの表示 */}
+          <Box
+            sx={{
+              position: "relative",
+              pl: 4,
+              "&::before": {
+                content: '""',
+                position: "absolute",
+                left: "12px",
+                top: "8px",
+                bottom: "8px",
+                width: "2px",
+                background:
+                  "linear-gradient(to bottom, #9333EA 0%, #DB2777 100%)",
+                borderRadius: "1px",
+              },
+            }}
+          >
+            {/* 出発地点 */}
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                mb: 1.5,
+                position: "relative",
+              }}
+            >
+              <Box
+                sx={{
+                  position: "absolute",
+                  left: "-28px",
+                  backgroundColor: "#9333EA",
+                  borderRadius: "50%",
+                  p: 0.5,
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Home className="w-4 h-4 text-white" />
+              </Box>
+              <Typography
+                variant="body2"
+                sx={{
+                  color: "#9333EA",
+                  fontWeight: "medium",
+                }}
+              >
+                {postData.origin_name}
+              </Typography>
+            </Box>
+
+            {/* ポイント1 */}
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                mb: 1.5,
+                position: "relative",
+              }}
+            >
+              <Box
+                sx={{
+                  position: "absolute",
+                  left: "-28px",
+                  backgroundColor: "#AB4BED",
+                  borderRadius: "50%",
+                  p: 0.5,
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <MapPin className="w-4 h-4 text-white" />
+              </Box>
+              <Typography
+                variant="body2"
+                sx={{
+                  color: "#AB4BED",
+                  fontWeight: "medium",
+                }}
+              >
+                {postData.point1_name}
+              </Typography>
+            </Box>
+
+            {/* ポイント2 */}
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                mb: 1.5,
+                position: "relative",
+              }}
+            >
+              <Box
+                sx={{
+                  position: "absolute",
+                  left: "-28px",
+                  backgroundColor: "#C162F0",
+                  borderRadius: "50%",
+                  p: 0.5,
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Navigation className="w-4 h-4 text-white" />
+              </Box>
+              <Typography
+                variant="body2"
+                sx={{
+                  color: "#C162F0",
+                  fontWeight: "medium",
+                }}
+              >
+                {postData.point2_name}
+              </Typography>
+            </Box>
+
+            {/* ポイント3 */}
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                position: "relative",
+              }}
+            >
+              <Box
+                sx={{
+                  position: "absolute",
+                  left: "-28px",
+                  backgroundColor: "#DB2777",
+                  borderRadius: "50%",
+                  p: 0.5,
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Flag className="w-4 h-4 text-white" />
+              </Box>
+              <Typography
+                variant="body2"
+                sx={{
+                  color: "#DB2777",
+                  fontWeight: "medium",
+                }}
+              >
+                {postData.point3_name}
+              </Typography>
+            </Box>
+          </Box>
         </Box>
       </Box>
     </Paper>
